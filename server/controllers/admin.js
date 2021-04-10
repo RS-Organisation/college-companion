dde8a845b318dd4f7f854a808113ba5065f3f9a3
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-04-10 17:36:39 +0530

    add routes in admin

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,5 +1,19 @@
 const Admin = require('../models/admin');
+const Faculty = require('../models/faculty');
+const Student = require('../models/student');
 const mongoose = require('mongoose');
+
+// GET ROUTES
+
+const getAllAdmins = async (req, res) => {
+  try {
+    const details = await Admin.find({});
+    res.status(200).json(details);
+    // console.log(postMessages);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 const getAdminDetails = async (req, res) => {
   try {
@@ -12,7 +26,28 @@ const getAdminDetails = async (req, res) => {
   }
 };
 
-const createAdmin = async (req, res) => {
+const getAllFaculty = async (req, res) => {
+  try {
+    const details = await Faculty.find({});
+    console.log(details);
+    res.status(200).json(details);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+const getAllStudents = async (req, res) => {
+  try {
+    const details = await Student.find({});
+    res.status(200).json(details);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// POST ROUTES
+
+const addAdmin = async (req, res) => {
   const details = req.body;
   try {
     const newAdmin = new Admin({
@@ -26,4 +61,40 @@ const createAdmin = async (req, res) => {
   }
 };
 
-module.exports = { getAdminDetails, createAdmin };
+const addFaculty = async (req, res) => {
+  const details = req.body;
+  try {
+    const newFaculty = new Faculty({
+      ...details,
+      createdAt: new Date().toISOString(),
+    });
+    await newFaculty.save();
+    res.status(201).json(newFaculty);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+const addStudent = async (req, res) => {
+  const details = req.body;
+  try {
+    const newStudent = new Student({
+      ...details,
+      createdAt: new Date().toISOString(),
+    });
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getAllAdmins,
+  getAdminDetails,
+  getAllFaculty,
+  getAllStudents,
+  addAdmin,
+  addFaculty,
+  addStudent,
+};
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAdminDetails, createAdmin } = require('../controllers/admin');
+const {
+  getAllAdmins,
+  getAdminDetails,
+  getAllFaculty,
+  getAllStudents,
+  addAdmin,
+  addFaculty,
+  addStudent,
+} = require('../controllers/admin');
 
+router.get('/all', getAllAdmins);
 router.get('/:id', getAdminDetails);
-router.post('/create', createAdmin);
+router.get('/allFaculty', getAllFaculty);
+router.get('/allStudents', getAllStudents);
+router.post('/addAdmin', addAdmin);
+router.post('/addFaculty', addFaculty);
+router.post('/addStudent', addStudent);
 
 module.exports = router;
