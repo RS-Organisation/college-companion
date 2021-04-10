4991c71507664d7553e81feb2c5a12a311da335b
Roopin Bhadri <bhadriroopin748@gmail.com>
2021-04-10 16:28:21 +0530

    Modify database connection URI

--- server/index.js
+++ server/index.js
@@ -1,39 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+require('./db/mongoose.js');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config();
-const app = express();
-
+// importing routes
 const adminRoutes = require('./routes/admin');
-// const userRoutes = require('./routes/users');
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors()); //use cors middleware
 app.use(express.json()); //to parse json data that our server sending and receiving
 
 app.use('/admin', adminRoutes);
-// app.use('/user', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
-});
-
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-  console.log('MongoDB database connection establised successfully');
-});
-
-mongoose.set('useFindAndModify', false);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
 });
