2f52438359173b4e59cac7d81469ad5317d982f1
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-04-22 22:38:32 +0530

    generate enrollment number

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,42 +1,52 @@
 const Student = require('../models/student');
 const mongoose = require('mongoose');
+const { generateEnrollmentNumber } = require('../util/helperFunctions');
 
 // GET ROUTES
 
 const getStudentDetails = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const details = await Student.findById(id);
-		res.status(200).json(details);
-	} catch (err) {
-		res.status(404).json({ message: err.message });
-	}
+  try {
+    const { id } = req.params;
+    const details = await Student.findById(id);
+    res.status(200).json(details);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 const getAllStudents = async (req, res) => {
-	try {
-		const details = await Student.find({});
-		res.status(200).json(details);
-	} catch (err) {
-		res.status(404).json({ message: err.message });
-	}
+  try {
+    const details = await Student.find({});
+    res.status(200).json(details);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 // POST ROUTES
 
 const addStudent = async (req, res) => {
-	const details = req.body;
-	try {
-		const newStudent = new Student({ ...details });
-		await newStudent.save();
-		res.status(201).json(newStudent);
-	} catch (err) {
-		res.status(400).json({ message: err.message });
-	}
+  const details = req.body;
+  try {
+    const count = await Student.countDocuments({
+      department: details.department,
+    });
+    const prefix = details.department;
+    const enrollmentNumber = generateEnrollmentNumber(
+      prefix,
+      details.joiningYear,
+      count + 1
+    );
+    const newStudent = new Student({ ...details, enrollmentNumber });
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 module.exports = {
-	getStudentDetails,
-	getAllStudents,
-	addStudent
+  getStudentDetails,
+  getAllStudents,
+  addStudent,
 };
--- server/util/helperFunctions.js
+++ server/util/helperFunctions.js
@@ -1,11 +1,20 @@
 // Function to Generate Registration Number for Admin and Faculty
 const generateRegistrationNumber = (prefix, joiningYear, count) => {
-    const formattedCount = count.toLocaleString('en-US', {
-        minimumIntegerDigits: 3,
-        useGrouping: false
-    });
-    const registrationNumber = prefix + String(joiningYear) + formattedCount;
-    return registrationNumber;
+  const formattedCount = count.toLocaleString('en-US', {
+    minimumIntegerDigits: 3,
+    useGrouping: false,
+  });
+  const registrationNumber = prefix + String(joiningYear) + formattedCount;
+  return registrationNumber;
 };
 
-module.exports = { generateRegistrationNumber };
+const generateEnrollmentNumber = (prefix, joiningYear, count) => {
+  const formattedCount = count.toLocaleString('en-US', {
+    minimumIntegerDigits: 3,
+    useGrouping: false,
+  });
+  const enrollmentNumber = String(joiningYear) + prefix + formattedCount;
+  return enrollmentNumber;
+};
+
+module.exports = { generateRegistrationNumber, generateEnrollmentNumber };
