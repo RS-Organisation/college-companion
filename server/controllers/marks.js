54f0d6b5d658a08cdc52b22a8c0298afdf1a45f7
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-04-23 22:42:25 +0530

    resolve errors in marks route

--- server/controllers/marks.js
+++ server/controllers/marks.js
@@ -5,28 +5,27 @@ const mongoose = require('mongoose');
 const uploadMarks = async (req, res) => {
   try {
     const {
-      section, 
-      department, 
-      subjectCode, 
-      examType, 
-      totalMarks, 
+      section,
+      department,
+      subjectCode,
+      examType,
+      totalMarks,
       studentsList,
     } = req.body;
 
-    const sub = await Subject.find({ subjectCode });
+    const sub = await Subject.findOne({ subjectCode });
 
     const isUploaded = await Marks.findOne({
+      subject: sub._id,
       examType,
       section,
       department,
-      subject: sub._id,
     });
 
     if (isUploaded) {
       const errorMessage = 'You have already uploaded marks of this exam';
       return res.status(400).json({ message: errorMessage });
     }
-
     studentsList.forEach(async (stud) => {
       const newMarks = await new Marks({
         student: stud.studentId,
@@ -35,13 +34,12 @@ const uploadMarks = async (req, res) => {
         totalMarks,
         section,
         department,
-        marks: stud.marks
+        marks: stud.marks,
       });
       await newMarks.save();
-    });    
-    res.status(201).json({ message: 'Marks uploaded successfully.' });
-  } 
-  catch (err) {
+    });
+    return res.status(201).json({ message: 'Marks uploaded successfully.' });
+  } catch (err) {
     const errorMessage = `Error in uploading marks : ${err.message}`;
     return res.status(400).json({ message: errorMessage });
   }
